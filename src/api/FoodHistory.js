eb62dd0968068bf1fb1cd761f017a8756aebb1ca
kmh <micic1415@gmail.com>
2026-03-06 17:38:13 +0900

    fri

--- src/api/FoodHistory.js
+++ src/api/FoodHistory.js
@@ -4,7 +4,7 @@ export const meals = () => {
       label: '아침',
       image: '/images/food.jpg',
       items: [
-        { name: '닭가슴살', kcal: 450 },
+        { name: '닭가슴살', kcal: 1450 },
         { name: '샐러드', kcal: 200 },
         { name: '오렌지', kcal: 320 },
       ],
