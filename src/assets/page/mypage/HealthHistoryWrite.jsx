c01635059fbdc2899f0812913425bb85b4bf11f8
hakchulk <hakchulk@gmail.com>
2026-03-11 17:16:51 +0900

    0311 hak HealthHistoryWrite.jsx 대소문자문제

--- src/assets/page/mypage/HealthHistoryWrite.jsx
+++ src/assets/page/mypage/HealthHistoryWrite.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import BtnComp from '../../../components/BtnComp';
-import { getExercises } from '../../../api/exercise';
+import { getExercises } from '../../../api/Exercise';
 import {
   calculateWorkout,
   createWorkout,
